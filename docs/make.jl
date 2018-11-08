98128f7ab6dfc77ed25eaa344996af632751e2d0
Judd Mehr <juddmehr@gmail.com>
2018-11-08 10:37:52 -0700

    try some other stuff to get docs to deploy

--- docs/make.jl
+++ docs/make.jl
@@ -8,19 +8,10 @@ makedocs(
     sitename = "Splines.jl",
 )
 
-# deploydocs(
-#     deps   = Deps.pip("mkdocs", "python-markdown-math"),
-#     repo   = "github.com/juddmehr/Splines.jl.git",
-#     branch = "gh-pages",
-#     latest = "master",
-#     julia  = "0.7",
-#     osname = "osx"
-#     )
-
 deploydocs(
+    repo   = "github.com/byuflowlab/Splines.jl",
     root   = ".",
     target = "./build",
-    repo   = "github.com/byuflowlab/Splines.jl",
     branch = "gh-pages",
     devbranch = "master",
 )
