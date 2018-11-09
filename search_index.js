a09ec1863e02494a54b00e944c1ea94b7abc1131
Judd Mehr <juddmehr@gmail.com>
2018-11-09 07:44:42 -0700

    Deployed 72340b9 with MkDocs version: 1.0.3

--- search_index.js
+++ search_index.js
@@ -173,7 +173,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Splines.jl",
     "title": "Functions",
     "category": "section",
-    "text": "Descriptions of available methods along with example implementations can be found on the functions page.Pages = [\"Functions.md\"]\nDepth = 2##AboutPages = [\"../../LICENSE.md\"]"
+    "text": "Descriptions of available methods along with example implementations can be found on the functions page.Pages = [\"Functions.md\"]\nDepth = 2"
+},
+
+{
+    "location": "#About-1",
+    "page": "Splines.jl",
+    "title": "About",
+    "category": "section",
+    "text": "Pages = [\"license.md\"]"
+},
+
+{
+    "location": "license/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "The Splines.jl package is licensed under the MIT \"Expat\" License:Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
 },
 
 ]}
