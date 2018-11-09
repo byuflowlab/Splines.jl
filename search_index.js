e0a80fdde823b9aa6e9c2e4fc62eee328b9ca05a
Judd Mehr <juddmehr@gmail.com>
2018-11-09 07:22:18 -0700

    Deployed 73a3d7a with MkDocs version: 1.0.3

--- search_index.js
+++ search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Functions",
     "title": "Functions",
     "category": "section",
-    "text": "The majority of the functions listed below come from The NURBS Book by Les Piegl and Wayne Tiller, hereafter referred to as \"NURBS\" when referencing equations and/or algorithms."
+    "text": ""
 },
 
 {
@@ -154,18 +154,26 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#",
-    "page": "Splines.jl Documentation",
-    "title": "Splines.jl Documentation",
+    "page": "Splines.jl",
+    "title": "Splines.jl",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "#Splines.jl-Documentation-1",
-    "page": "Splines.jl Documentation",
-    "title": "Splines.jl Documentation",
+    "location": "#Splines.jl-1",
+    "page": "Splines.jl",
+    "title": "Splines.jl",
     "category": "section",
-    "text": "CurrentModule = SplinesPages = [\"Functions.md\"]\nDepth = 2"
+    "text": "CurrentModule = SplinesSplines.jl is a work-in-progress splines package focusing on Bézier curves, Basis Splines (B-Splines), and Non-uniform Rational B-Splines (NURBS). As of now, the available methods are transcribed from algorithms, or created from equations found in The NURBS Book by Les Piegl and Wayne Tiller, hereafter referred to as \"NURBS\" when referencing equations and/or algorithms.The algorithms outlined in the text are for C/C++ code. The C language, and the theory as presented is zero indexed. In an effort to preserve the algorithms as displayed in the text, and to keep the native Julia 1-indexing, indices are typically left as-is from the text with the addition of a +1.For FLOW Lab students, a quick intro to pertinent spline theory can be found in the FLOW Lab Notebook, but for in depth explanation The NURBS Book is the recommended resource.Note that this package has been created primarily for research purposes, so only basic tools required for that research have been implemented at this time. As the research progresses, more methods will be added as required."
+},
+
+{
+    "location": "#Functions-1",
+    "page": "Splines.jl",
+    "title": "Functions",
+    "category": "section",
+    "text": "Descriptions of available methods along with example implementations can be found on the functions page.Pages = [\"Functions.md\"]\nDepth = 2"
 },
 
 ]}
